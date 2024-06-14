86674f59cf98e2e950b6eb033d5423163d040a93
mitkooooooooooooooooooooooo <2008mitko@gmail.com>
2024-06-14 19:41:14 +0300

    хдшунуу

---
+++ tennisscoreapp/tennisscoreapp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tennisscoreapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
